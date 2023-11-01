540e431e40e27195a9d552cd095443c07783ae59
Danilo0203 <50729253+Danilo0203@users.noreply.github.com>
2023-11-01 15:00:40 -0600

    deploy

--- src/public/js/index.js
+++
@@ -1,7 +0,0 @@
-// Import our custom CSS
-import '../scss/styles.scss';
-import Alert from 'bootstrap/js/dist/alert';
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap';
-import { App } from './app';
-App('body');
