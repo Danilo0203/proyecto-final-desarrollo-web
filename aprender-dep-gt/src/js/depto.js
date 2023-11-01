7555a5e2bfc16e375f75c8f2506cb6044c0e7677
Danilo0203 <50729253+Danilo0203@users.noreply.github.com>
2023-11-01 12:40:47 -0600

    Aprender Depto terminado

---
+++ aprender-dep-gt/src/js/depto.js
@@ -0,0 +1,12 @@
+import html from '../depto.html?raw';
+import { navBar } from './cases-uses/nav-opacity';
+import { seleccionarDepto } from './cases-uses/seleccionarDepto';
+
+export const Depto = (elementId) => {
+	(() => {
+		const app = document.querySelector(elementId);
+		app.innerHTML = html;
+	})();
+};
+navBar();
+seleccionarDepto();
